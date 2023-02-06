8e23b5685aa4cb524a4ee8250115abde9a43d4f1
tomasda7 <tomasda7@outlook.com>
2023-02-06 15:21:43 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Ejercicio 1
+
+string GetName(string name) { return name; }
+
+Console.WriteLine(GetName("Tomás"));
+
+//Ejercicio 2
+
+string GetHour() {
+    string time = DateTime.Now.ToString("T");
+
+    return "La hora actual es " + time;
+}
+
+Console.WriteLine(GetHour());
